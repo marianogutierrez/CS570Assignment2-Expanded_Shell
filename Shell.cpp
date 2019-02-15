5ad6bfa78453d5caf748de27edfe014ee298936d
Mariano Gutierrez <margutierrez75@gmail.com>
2019-02-14 19:10:12 -0800

    testing

--- Shell.cpp
+++ Shell.cpp
@@ -14,7 +14,7 @@
 
 using namespace std;
 
-const int MAX_ARGS = 100; // so, really 99 args max since first arg is the program to run
+const int MAX_ARGS = 102; // so, really 99 args max since first arg is the program to run
 // Cameron Ozatalar
 // Mariano Gutierrez
 
@@ -22,12 +22,14 @@ const int MAX_ARGS = 100; // so, really 99 args max since first arg is the progr
   3Method to freeup the allocated array of parameters
  free things in order reverse stack-wise LIFO
  */
+/*
 void freeLines(char** buff) {
-  for(int i = 0; i < 256; i++) {
+  for(int i = 0; i < MAX_ARGS; i++) {
    free(buff[i]);
   }
   free(buff);
 }
+*/
 
 void execute_commands(LinkedList* list){
     while(list -> size != 0){
@@ -39,12 +41,6 @@ void execute_commands(LinkedList* list){
 
         }else if(token -> compare("&") == 0){          
             cout << "IO redirection and background not implemented" << endl;
-
-        }else if(token -> compare("|") == 0){ // might not need this 
-            cout << "Pipe not implemented" << endl; // should pretend it is a ";" refer to bottom of pg.3
-
-       // }else if(token -> compare(";") == 0){ // what do we need to do for this again?
-            // should indicate to just continue processing the commands 
 
          } else if(token -> compare("cd") == 0){
             // TODO: ASK e.g. cd /media/ | ; goto media then print fail  NOTE BOTTOM OF PG 3 says this is required 
@@ -67,19 +63,17 @@ void execute_commands(LinkedList* list){
         else {
             // fork-exec 
             char** arrayOfParameters = (char**) malloc((sizeof(char*) * MAX_ARGS));
-            for(int i = 0; i < 256; i++) { // 2-d array
+            for(int i = 0; i < MAX_ARGS; i++) { // 2-d array
                 arrayOfParameters[i] = (char*) malloc(sizeof(char) * 256); // max length of the string will be 256 bytes
             }
             strcpy(arrayOfParameters[0],token -> c_str()); // first arg ought to the name of the program
             arrayOfParameters[MAX_ARGS-1] = '\0'; // Last arguement must be null as requested to function with execvp
 
-            //TODO Produce error message if arg length exceeded!
-            // will probably neeed to consume tokens until empty or hit "; or |"
-            // check is greater than max length then and check for errors 
+
             int currentSize = list -> size;
-            for(int i = 1; i < currentSize; i++) {
+            for(int i = 1; i < currentSize; i++) { // for filling array of params
                 // check max arg amount...
-                 if(i == 99) {
+                 if(i == 101) {
                     cout << "Max number of args reached!" << endl;
                     break;
                 }
@@ -90,7 +84,7 @@ void execute_commands(LinkedList* list){
                     cout << "Pipe not implemented" << endl; // warn the user.
                     break; // continue i.e. treat as a ;
                 }
-            
+                
                 strcpy(arrayOfParameters[i], token -> c_str());
             }
                 
@@ -113,7 +107,7 @@ void execute_commands(LinkedList* list){
             else {
             cout << "Process exited successfully" << endl;
             }
-            freeLines(arrayOfParameters); // either way, we need to free that array of pointers 
+            //freeLines(arrayOfParameters); // either way, we need to free that array of pointers 
         } 
     }
 }
